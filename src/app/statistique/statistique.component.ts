c0d48025adf91048efdc0ff6c1cb6b2ad399ea5a
Kokuga <damien.mizzi@live.fr>
2021-07-30 18:42:50 +0200

    Exercice 7

--- src/app/statistique/statistique.component.ts
+++ src/app/statistique/statistique.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Statistique} from '../../models/statistique';
 import {StatistiqueService} from '../statistique.service';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-statistique',
@@ -11,6 +12,10 @@ import {HttpClient} from "@angular/common/http";
 export class StatistiqueComponent implements OnInit {
 
   public StatsArray!: Statistique[];
+  public id = '';
+  public title = '';
+  public value = '';
+  public appreciation = '';
 
   constructor(private Stats: StatistiqueService, private http: HttpClient) {
 
@@ -29,6 +34,18 @@ export class StatistiqueComponent implements OnInit {
       .then(() => {
         let removedIndex = this.StatsArray.findIndex(stat => stat.id === id);
         this.StatsArray.splice(removedIndex, 1);
+      });
+  }
+
+  submit() {
+    this.http.post('https://stats.naminilamy.fr', {
+      id: this.id,
+      value: this.value,
+      title: this.title,
+      appreciation: this.appreciation
+    })
+      .subscribe((data: any) => {
+        this.StatsArray.push(new Statistique(data.id, data.value, data.title, data.appreciation));
       });
   }
 }
